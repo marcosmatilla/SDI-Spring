aee4a34d1252b069a03f89932655807e1cc221c9
UO258935 <UO258935@uniovi.es>
2020-02-11 15:05:53 +0100

    SDI-session2-Ejercicio-complementario3

--- sdi1920-510-lab-spring/src/main/java/com/uniovi/controllers/ProffesorsController.java
+++ sdi1920-510-lab-spring/src/main/java/com/uniovi/controllers/ProffesorsController.java
@@ -1,41 +1,49 @@
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.uniovi.entities.Proffesor;
 import com.uniovi.services.ProffesorsService;
 
-@RestController
+@Controller
 public class ProffesorsController{
 	
 	@Autowired
 	private ProffesorsService proffesorService;
 
 	@RequestMapping("/proffesor/list")
-	public String getList() {
-		return proffesorService.getProffesors().toString();
+	public String getList(Model model) {
+		model.addAttribute("proffesorList", proffesorService.getProffesors());
+		return "proffesor/list";
+	}
+	
+	@RequestMapping(value="/proffesor/add")
+	public String getProffesor() {
+		return "proffesor/add";
 	}
 	
 	@RequestMapping(value = "/proffesor/add", method=RequestMethod.POST)
 	public String setProffesor(@ModelAttribute Proffesor proffesor) {
 		proffesorService.addProffesor(proffesor);
-		return "Proffesor added";
+		return "redirect:/proffesor/list";
 	}
 	
 	@RequestMapping("/proffesor/details/{dni}" )
-	public String getDetail(@PathVariable Long dni) {
-		return proffesorService.getProffesor(dni).toString();
+	public String getDetail(Model model, @PathVariable Long dni) {
+		model.addAttribute("proffesor", proffesorService.getProffesor(dni));
+		return "proffesor/details";
 	}
 	
 	@RequestMapping("/proffesor/delete/{dni}" )
 	public String deleteProffesor(@PathVariable Long dni) {
 		proffesorService.deleteProffesor(dni);
-		return "Proffesor deleted";
+		return "redirect:/proffesor/list";
 	}
 
 }
