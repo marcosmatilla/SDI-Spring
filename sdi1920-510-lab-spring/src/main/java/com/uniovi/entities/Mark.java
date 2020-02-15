4042738af6f480c072070a9a6c7a4552750e8fa3
UO258935 <UO258935@uniovi.es>
2020-02-15 19:27:59 +0100

    SDI-SpringBoot- 4 -Acceso a datos con entidades relacionadas

--- sdi1920-510-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi1920-510-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -1,4 +1,5 @@
 package com.uniovi.entities;
+
 import javax.persistence.*;
 
 @Entity
@@ -9,6 +10,10 @@ public class Mark {
 	private String description;
 	private Double score;
 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	public Mark() {
 	}
 
@@ -17,6 +22,13 @@ public class Mark {
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
 	}
 
 	public Long getId() {
@@ -41,6 +53,14 @@ public class Mark {
 
 	public void setScore(Double score) {
 		this.score = score;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 	@Override
